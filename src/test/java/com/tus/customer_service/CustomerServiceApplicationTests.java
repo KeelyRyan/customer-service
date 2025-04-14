da239354ff5f48b67322c6091f5f2849ccc5030e
Keely Ryan <keelyannryan@gmail.com>
2025-04-14 21:23:58 +0100

    initial commit

---
+++ src/test/java/com/tus/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
