2ae03d74f73a0ed3ff356d99a000d782f1e4bc0c
Keely Ryan <keelyannryan@gmail.com>
2025-04-19 20:54:34 +0100

    Add customer service to repo

--- src/main/java/com/tus/customer/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tus.customer.exception;
-
-@SuppressWarnings("serial")
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resource, String field, String value) {
-        super(String.format("%s not found with %s: '%s'", resource, field, value));
-    }
-
-}
